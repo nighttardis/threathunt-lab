13bb1cf3ce58f6b33cfacfdaf6601dad91659380
nighttardis <nighttardis1701@gmail.com>
2022-04-22 15:04:27 -0500

    finished installing inetsim, polarproxy and tcpreplay

---
+++ terraform/lab.tf
@@ -0,0 +1,168 @@
+# Proxmox Full-Clone
+# ---
+# Create a new VM from a clone
+
+variable "packer_ssh_key" {
+    type = string
+}
+
+variable "proxmox_host" {
+    type = string
+}
+
+variable "debian_source" {
+    type = string
+}
+
+resource "proxmox_vm_qemu" "debian-inetsim" {
+    
+    # VM General Settings
+    target_node = var.proxmox_host
+    vmid = "300"
+    name = "debian-inetsim"
+
+    # VM Advanced General Settings
+    onboot = false 
+
+    # VM OS Settings
+    clone = var.debian_source
+
+    # VM System Settings
+    agent = 1
+    
+    # VM CPU Settings
+    cores = 2
+    sockets = 1
+    cpu = "host"    
+    
+    # VM Memory Settings
+    memory = 2048
+
+    # VM Network Settings
+    network {
+        bridge = "vmbr0"
+        model  = "virtio"
+    }
+
+    network {
+        bridge = "vmbr1"
+        model  = "virtio"
+    }
+
+    network {
+        bridge = "vmbr1"
+        model  = "virtio"
+    }
+
+    lifecycle {
+        ignore_changes = [
+            network, qemu_os, disk_gb, desc
+        ]
+    }
+
+    # VM Cloud-Init Settings
+    os_type = "cloud-init"
+
+    # (Optional) IP Address and Gateway
+    # ipconfig0 = "ip=0.0.0.0/0,gw=0.0.0.0"
+    ipconfig1 = "ip=10.0.1.2/24"
+    
+    # (Optional) Default User
+    # ciuser = "user_name"
+    
+    # (Optional) Add your SSH KEY
+    # sshkeys = <<EOF
+    # SSH KEY
+    # EOF
+
+    connection {
+      type = "ssh"
+      user = "packer"
+      host = self.ssh_host
+      private_key = "${file(var.packer_ssh_key)}"
+    }
+
+    provisioner "file" {
+        source = "files/packages/inetsim.list"
+        destination = "/tmp/inetsim.list"
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+          "sudo mv /tmp/inetsim.list etc/apt/sources.list.d/inetsim.list",
+          "wget -O /tmp/inetsim-archive-signing-key.asc https://www.inetsim.org/inetsim-archive-signing-key.asc",
+          "sudo apt-key add /tmp/inetsim-archive-signing-key.asc",
+          "sudo rm /tmp/inetsim-archive-signing-key.asc"
+      ]
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo apt update",
+        "sudo apt install -y curl gnupg inetsim"
+      ]
+    }
+
+    provisioner "file" {
+        source = "files/interface.d/49-sniff"
+        destination = "/tmp/49-sniff"
+      
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+          "sudo cp /tmp/49-sniff /etc/network/interfaces.d/49-sniff"
+        ]
+    }
+
+    # Configure inetsim
+    provisioner "remote-exec" {
+        inline = [
+          "sudo sed -i 's/^#service_bind_address.*/service_bind_address 10.0.1.2/' /etc/inetsim/inetsim.conf",
+          "sudo sed -i 's/^start_service https/#start_service https/' /etc/inetsim/inetsim.conf",
+          "sudo systemctl start inetsim",
+          "sudo systemctl enable inetsim"
+        ]
+    }
+
+    # Configure PolarProxy
+    provisioner "file" {
+      source = "files/PolarProxy/PolarProxy.service"
+      destination = "/tmp/PolarProxy.service"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+          "mkdir /home/packer/PolarProxy",
+          "curl https://www.netresec.com/?download=PolarProxy -o /home/packer/PolarProxy/PolarProxy.tar.gz",
+          "sudo mkdir -p /usr/local/bin/PolarProxy",
+          "sudo tar -xzf /home/packer/PolarProxy/PolarProxy.tar.gz -C /usr/local/bin/PolarProxy",
+          "sudo mv /tmp/PolarProxy.service /etc/systemd/system/PolarProxy.service",
+          "sudo mkdir -p /var/log/PolarProxy",
+          "sudo systemctl daemon-reload",
+          "sudo systemctl start PolarProxy",
+          "sudo systemctl enable PolarProxy"
+        ]
+    }
+
+    # Configure TCPReplay
+    provisioner "file" {
+        source = "files/tcpreplay/tcpreplay.service"
+        destination = "/tmp/tcpreplay.service"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo apt install -y tcpreplay",
+            "sudo mv /tmp/tcpreplay.service /etc/systemd/system/tcpreplay.service",
+            "sudo systemctl daemon-reload",
+            "sudo systemctl start tcpreplay",
+            "sudo systemctl enable tcpreplay"            
+        ]      
+    }
+
+    provisioner "local-exec" {
+        command = "echo Run \"ovs-vsctl -- --id=@p get port tap${self.vmid}i2 -- --id=@m create mirror name=span1 select-all=true output-port=@p -- set bridge vmbr1 mirrors=@m\" on ${var.proxmox_host}"  
+    }
+
+}
